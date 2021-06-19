fd37f6f721d204be40dbec6c1f3d1900b3a135ad
Nagalakshmi Selvaraj <lakshmi.padumanabhan@gmail.com>
2021-06-19 21:26:03 +1000

    initial cucumber setup added

---
+++ step_definitions/hooks.js
@@ -0,0 +1,30 @@
+const {
+    BeforeAll,
+    AfterAll,
+    Before,
+    After,
+    setDefaultTimeout
+} = require('@cucumber/cucumber')
+
+const puppeteer = require('puppeteer')
+
+BeforeAll(async function () {
+
+})
+
+Before(async function () {
+
+})
+
+After(async function () {
+
+})
+
+AfterAll(async function () {
+
+})
+
+//Set the global modules here
+const setGlobalVariables = () => {
+
+}
