53c74d9e523071dd434562b03473c48b708753ba
Nagalakshmi Selvaraj <lakshmi.padumanabhan@gmail.com>
2021-06-19 21:36:55 +1000

    winston logger configured

--- step_definitions/hooks.js
+++ step_definitions/hooks.js
@@ -9,7 +9,8 @@ const {
 const puppeteer = require('puppeteer')
 
 BeforeAll(async function () {
-
+    setGlobalVariables()
+    logger.info("Before starting test suite...")
 })
 
 Before(async function () {
@@ -26,5 +27,19 @@ AfterAll(async function () {
 
 //Set the global modules here
 const setGlobalVariables = () => {
+    global.logger = configureLogger()
+}
 
+const configureLogger = () => {
+
+    const winston = require('winston')
+
+    return winston.createLogger({
+        level: 'info',
+        format: winston.format.simple(),
+        transports: [
+            new winston.transports.Console(),
+            new winston.transports.File({ filename: 'combined.log' })
+        ]
+    })
 }
